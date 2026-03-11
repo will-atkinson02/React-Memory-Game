ce255574ccd00972fd860480debc749055d90bf4
unknown <willreubena@gmail.com>
2026-03-11 15:32:32 +0000

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Card from './components/Card'
+import "./index.css"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="text-5xl">hello</div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,6 @@
+export default function Card()
+{
+    return (
+        <div className="w-6 h-6 border-2 border-red-500"></div>
+    )
+}
