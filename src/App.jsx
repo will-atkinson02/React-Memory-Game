d492e58df48b43cfb90a4349eb4846794a44f73f
unknown <willreubena@gmail.com>
2026-03-26 17:03:22 +0000

    Added attempt counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import "./index.css"
 import GameBoard from "./components/GameBoard"
+import { useState } from "react"
 
 function App() {
+
+  const [attempts, setAttempts] = useState(0)
+
+  function incrementAttempts()
+  {
+    setAttempts(attempts + 1)
+  }
 
   return (
     <>
       <div className="flex justify-center items-center h-screen">
-        <GameBoard></GameBoard>
+        <div className="absolute top-8">Attempts: {attempts}</div>
+        <GameBoard incrementAttempts={incrementAttempts}></GameBoard>
       </div>
     </>
   )
